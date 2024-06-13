ec373b4ccf1e90b0debbeb1e1d9709d1e1c6d4cb
Noah Thornton <noahthornton15@gmail.com>
2024-06-12 23:27:33 -0700

    feat: add foreign-env to fish
    This onboards fish shell to use foreign-env which is required to
    propoerly setup paths on fish shell.

--- common/home-manager/home.nix
+++ common/home-manager/home.nix
@@ -4,6 +4,16 @@
 
   programs.fish = {
     enable = true;
+    plugins = [
+      {
+        name = "fenv";
+        src = pkgs.fishPlugins.foreign-env;
+      }
+    ];
+    shellInit = "
+      set -p fish_function_path ${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d\n
+      fenv source ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh > /dev/null
+    ";
   };
 
 
