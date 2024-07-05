b55642a124d560fca06daca38f674ed0dc457bb9
Noah Thornton <noahthornton15@gmail.com>
2024-07-05 00:12:50 -0700

    refactor(tmux): modularize tmux home-manager config

--- common/home-manager/home.nix
+++ common/home-manager/home.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./neovim.nix
+    ./tmux.nix
   ];
 
   home.stateVersion = "24.05";
@@ -146,36 +147,6 @@
   programs.thefuck = {
     enable = true;
     enableFishIntegration = true;
-  };
-
-  # terminal multiplexer
-  programs.tmux = {
-    enable = true;
-    mouse = true;
-    sensibleOnTop = true;
-    keyMode = "vi";
-    extraConfig = ''
-      set -ag terminal-overrides ",xterm-256color:RGB"
-    '';
-
-    plugins = with pkgs.tmuxPlugins; [
-      # CPU/MEM/SWAP/IO usage in status bar
-      # TODO add to status bar
-      sysstat
-      # Gruvbox colors
-      gruvbox
-      # Highlight when using prefix key
-      # TODO add to status bar
-      prefix-highlight
-      # Fzf to manage tmux
-      tmux-fzf
-      # Fzf searching in buffer
-      fuzzback
-      # Pane navigation keybinds
-      pain-control
-      # Mouse configuration
-      better-mouse-mode
-    ];
   };
 
   # youtube downloader
