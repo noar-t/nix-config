d31040a00d036d9c2ed9c41133b908a3a2efe0f0
Noah Thornton <noahthornton15@gmail.com>
2024-07-11 20:17:46 -0700

    refactor(fish): move home-manager fish config to new file

--- common/home-manager/home.nix
+++ common/home-manager/home.nix
@@ -3,6 +3,7 @@
   imports = [
     ./neovim.nix
     ./tmux.nix
+    ./fish.nix
   ];
 
   home.stateVersion = "24.05";
@@ -21,71 +22,6 @@
 
     # show icons next to items
     icons = true;
-  };
-
-  # user-friendly shell
-  programs.fish = {
-    enable = true;
-    plugins = with pkgs.fishPlugins; [
-      {
-        # Import bash env
-        name = "fenv";
-        src = foreign-env.src;
-      }
-      {
-        # Color theme
-        name = "fish-gruvbox";
-        src = gruvbox.src;
-      }
-      {
-        # Auto close (),"",''
-        name = "autopair";
-        src = autopair.src;
-      }
-      {
-        # Colorize man pages
-        name = "colored-man";
-        src = colored-man-pages.src;
-      }
-      {
-        # Text expansions
-        name = "puffer";
-        src = puffer.src;
-      }
-      #{ TODO only in unstable
-      #  # Remove failed commands from history
-      #  name = "sponge";
-      #  src = sponge.src;
-      #}
-    ];
-
-    shellAliases = {
-      "cat" = "bat";
-      "tree" = "eza -T";
-    };
-
-    interactiveShellInit = ''
-      # Disable fish greetings
-      set fish_greeting
-
-      # Use vim keys
-      fish_vi_key_bindings
-
-      # Set prompt path to show more info
-      set -g fish_prompt_pwd_dir_length 3
-      set -g fish_prompt_pwd_full_dirs 3
-
-      # Source homebrew
-      if test -x /opt/homebrew/bin/brew
-        eval (/opt/homebrew/bin/brew shellenv)
-      end
-    '';
-
-    # use fenv to source nix path correctly
-    shellInit = "
-      set -p fish_function_path ${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d\n
-      fenv source ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh > /dev/null\n
-    ";
   };
 
   # fuzzy finder for shell history, files, etc
@@ -116,21 +52,6 @@
     enable = true;
   };
 
-  # vim type editor
-  #programs.neovim = {
-  #  enable = true;
-  #  defaultEditor = true;
-  #  # TODO once vimrc is sorted ill enable this
-  #  viAlias = true;
-  #  vimAlias = true;
-  #  vimdiffAlias = true;
-  #  plugins = with pkgs.vimPlugins; [
-  #    lens-vim
-  #    hardtime-nvim
-  #    which-key-nvim
-  #  ];
-  #};
-
   # its ssh...
   programs.ssh = {
     enable = true;
@@ -140,7 +61,7 @@
   # faster tldr
   programs.tealdeer = {
     enable = true;
-    # TODO add alias to tldr
+    settings.updates.auto_update = true;
   };
 
   # command typo fixer
