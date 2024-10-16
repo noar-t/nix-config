694c55b7fd6e7438d18084034daa3eb0f22957b1
Noah Thornton <noahthornton15@gmail.com>
2024-10-15 23:07:45 -0700

    feat: introduce hyprland module

---
+++ modules/home-manager/hyprland.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ...}:
+{
+  options.myHome.hyprland.enable = lib.mkEnableOption {
+    name = "personal hyprland config";
+  };
+
+  config = lib.mkIf config.myHome.hyprland.enable {
+    wayland.windowManager.hyprland = {
+      enable = true;
+    };
+
+    home.packages = with pkgs; [
+      hyprlock
+      wl-clipboard
+    ];
+
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+  };
+}
