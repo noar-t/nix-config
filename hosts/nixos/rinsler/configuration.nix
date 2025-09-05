dcb5138fb51aa3c7fc60b8704599f94871e7e587
Noah Thornton <noahthornton15@gmail.com>
2025-09-04 22:28:20 -0700

    feat(borg): add automated borg backup

--- hosts/nixos/rinsler/configuration.nix
+++ hosts/nixos/rinsler/configuration.nix
@@ -167,5 +167,27 @@
     };
   };
 
+  services.borgbackup.jobs.home-backup = {
+    startAt = "daily";
+
+    paths = [ "/home/noah" "/home/docker" "/var/lib/docker/" ];
+
+    encryption.passCommand = "cat /home/noah/borg_pass";
+    encryption.mode = "repokey";
+
+    environment.BORG_RSH = "ssh -i /home/noah/.ssh/id_ed25519";
+    environment.BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes";
+
+    repo = "ssh://noah@wyzevault:22/mnt/storage/borgbackup";
+    extraCreateArgs = "--verbose --stats";
+    compression = "auto,zstd";
+
+    prune.keep = {
+      daily = 7;
+      weekly = 4;
+      monthly = 6;
+    };
+  };
+
   system.stateVersion = "24.05";
 }
